b49cc4881e8e89918d1ae38c8eb20ea4eca5f45d
Maxime Moreillon <m.moreillon@gmail.com>
2023-06-23 07:25:15 +0900

    save button snackbar

--- src/components/GarmentUpdateButton.vue
+++ src/components/GarmentUpdateButton.vue
@@ -1,8 +1,27 @@
 <template>
-  <v-btn icon @click="update_garment()" :loading="loading">
-    <v-icon v-if="success" color="success">mdi-check</v-icon>
-    <v-icon v-else>mdi-content-save</v-icon>
-  </v-btn>
+  <div>
+
+    <v-btn icon @click="update_garment()" :loading="loading">
+      <v-icon v-if="success" color="success">mdi-check</v-icon>
+      <v-icon v-else>mdi-content-save</v-icon>
+    </v-btn>
+    <v-snackbar
+      :color="snackbar.color"
+      v-model="snackbar.visible"
+    >
+      {{snackbar.text}}
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          icon
+          v-bind="attrs"
+          @click="snackbar.visible = false"
+        >
+        <v-icon>mdi-close</v-icon>
+          
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
 </template>
 
 <script>
@@ -15,6 +34,11 @@ export default {
     return {
       loading: false,
       success: false,
+      snackbar: {
+        visible: false,
+        text: "",
+        color: 'success',
+      }
     }
   },
   mounted() {
@@ -23,7 +47,6 @@ export default {
   beforeDestroy() {
     document.removeEventListener("keydown", this.handle_keydown_events)
   },
-
   methods: {
     handle_keydown_events(e) {
       if (e.key === "s" && e.ctrlKey) {
@@ -37,13 +60,20 @@ export default {
       try {
         await this.axios.patch(url, this.garment)
         this.success = true
+        this.snackbar.text = "Garment updated"
+        this.snackbar.visible = true
+        this.snackbar.color = "success"
         setTimeout(() => {
           this.success = false
         }, 2000)
       } catch (error) {
         if (error.response) console.error(error.response.data)
         else console.error(error)
-        alert("Update failed")
+
+        this.snackbar.text = "Garment update failed"
+        this.snackbar.visible = true
+        this.snackbar.color = "error"
+
       } finally {
         this.loading = false
       }
